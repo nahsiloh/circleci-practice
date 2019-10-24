f616b6a7e5d3a1671a851e4f79b08b5f16b8e1e2
nahsiloh <lishan1512@gmail.com>
2019-10-24 15:51:31 +0800

    first commit

---
+++ server.test.js
@@ -0,0 +1,9 @@
+const request = require("supertest");
+const app = require("./app");
+
+describe("Hello World", () => {
+  it("/GET should return hello world", async () => {
+    const response = await request(app).get("/");
+    expect(response.text).toBe("Hello World");
+  });
+});
